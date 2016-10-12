8a1823b6747cabfd29eaa0507a9b27d759cfd1e2
azpiero <joek0624@gmail.com>
2016-10-12 12:24:43 +0900

    blushup

--- Arduino/PV_sender/PV_sender.ino
+++ Arduino/PV_sender/PV_sender.ino
@@ -1,5 +1,3 @@
-//送信側
-
 #include <wiring_private.h> //analogread高速化
 #define digitalPIN 9              // LEDはピン13に接続
 
@@ -12,7 +10,7 @@ int OFF_0 = 600; //お試し
 
 char Data[10]; //とりあえず10文字で
 int pData = 0;
-
+char crc[2]; //これもなんとかしたいな
 int flag = 1; //最初に到達したSerialか否か いらなそう
 
 void setup() {
@@ -22,80 +20,34 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) { //Serial.available()の返り値がデータのバイト数 charなら1byte 64までいける
-    Data[pData] = Serial.read(); //c
-    if (flag == 1) { //最初に必ずstartpulse活かせる data[0]はかならず最初のところだからいい
-      send_startpulse(3);
+    Data[pData] = Serial.read();
+    if(flag ==1){
+      delayMicroseconds(5000); //Serial.availableをbuffに格納するのに必要そうな時間
+      char len = (Serial.available() + '1') ; //文字数を取得
+      make_markpulse(); //
+      make_pulse(len,8); //lengthを送るため
       flag = 0;
-    } else {
-      send_message(0); //２文字目以降に必要
     }
-  } else {
-    flag = 1;
+    make_pulse(Data[pData],8); //
+    pData ++; //makepulseしたらインクリ
+    if(Serial.available() == 0) add_checksum();
   }
 }
 
-void send_startpulse(int n) {
-  while (n > 0) { //3pulseでスタート信号
+void make_markpulse() {
+  for(int n = 0; n<3; n++){
     digitalWrite(digitalPIN, HIGH);
     delayMicroseconds(ON_reader);
     digitalWrite(digitalPIN, LOW);
     delayMicroseconds(OFF_reader);
-    n --;
-  }
-  send_message(0);
-}
-
-void send_message(int n) {
-  if (n == 8){
-    if(Serial.available() == 0) {//ラスト
-      crc_pulse(crc()); //data長さは　pdata
-      //これをもとに信号を作成する必要あり 16bitの信号になるはず 
-      pData = 0;
-      Serial.println("");
-      send_end(3);
-    }else{
-      Serial.println("");
-      pData ++;
-    }
-  } else {
-    switch (Data[pData]>>(7-n) & 1) { //0回シフト 下1bitで場合分けしているイメッジ 違う
-      case 0:
-        digitalWrite(digitalPIN, HIGH);
-        delayMicroseconds(ON_0);
-        digitalWrite(digitalPIN, LOW);
-        delayMicroseconds(OFF_0);//
-        Serial.print("0"); //確認
-        send_message(n + 1);
-        break;
-      case 1:
-        digitalWrite(digitalPIN, HIGH);
-        delayMicroseconds(ON_1);
-        digitalWrite(digitalPIN, LOW);
-        delayMicroseconds(OFF_1);
-        Serial.print("1"); //確認
-        send_message(n + 1);
-        break;
-    }
   }
 }
 
-void send_end(int n) {
-  while (n > 0) { //3pulseでスタート信号
-    digitalWrite(digitalPIN, HIGH);
-    delayMicroseconds(ON_reader);
-    digitalWrite(digitalPIN, LOW);
-    delayMicroseconds(OFF_reader);
-    n --;
-   }
- }
-
-
-unsigned short crc(){
+void cal_crc(){
  unsigned int crc16 = 0xFFFFU; //初期値
  unsigned long i;
  int j;
-
- for ( i = 0 ; i <= pData ; i++ ){
+ for ( i = 0 ; i < pData ; i++ ){
    crc16 ^= (unsigned int)Data[i];
    for ( j = 0 ; j < 8 ; j++ ){
      if ( crc16 & 0x0001 ){
@@ -105,27 +57,37 @@ unsigned short crc(){
      }
    }
  }
- return (unsigned short)(crc16);
+ crc[0] = (crc16 >> 8) & 0xFFU;
+ crc[1] = crc16 & 0xFFU  ; //これが微妙
 }
 
-void crc_pulse(unsigned short crc16){
-  for(int i =0;i<16;i++){
-    switch(crc16>>(15-i) & 1){
-       case 0:
-        digitalWrite(digitalPIN, HIGH);
-        delayMicroseconds(ON_0);
-        digitalWrite(digitalPIN, LOW);
-        delayMicroseconds(OFF_0);//
-        Serial.print("0"); //確認
-        break;
-      case 1:
-        digitalWrite(digitalPIN, HIGH);
-        delayMicroseconds(ON_1);
-        digitalWrite(digitalPIN, LOW);
-        delayMicroseconds(OFF_1);
-        Serial.print("1"); //確認
-        break;      
-    }
-   }
+void make_pulse(char data, int n){ //
+  switch (data >>(n-1) & 1) { //0回シフト 下1bitで場合分けしているイメッジ 違う
+    case 0:
+      digitalWrite(digitalPIN, HIGH);
+      delayMicroseconds(ON_0);
+      digitalWrite(digitalPIN, LOW);
+      delayMicroseconds(OFF_0);//
+      Serial.print("0"); //確認
+      if(n > 1) make_pulse(data,n - 1);
+      break;
+    case 1:
+      digitalWrite(digitalPIN, HIGH);
+      delayMicroseconds(ON_1);
+      digitalWrite(digitalPIN, LOW);
+      delayMicroseconds(OFF_1);
+      Serial.print("1"); //確認
+      if(n > 1) make_pulse(data,n - 1);
+      break;
+  }
 }
 
+void add_checksum(){
+  cal_crc();
+  make_pulse(crc[0],8); //いまいち釈然としないけど笑
+  make_pulse(crc[1],8);
+  make_markpulse();
+  pData = 0;
+  flag = 1; 
+  Serial.println();
+}
