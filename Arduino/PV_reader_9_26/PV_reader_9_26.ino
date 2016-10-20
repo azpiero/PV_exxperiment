59384ba1f3e3ceab16c828ccb306fee398dd4ffa
youn410 <youn410@gmail.com>
2016-10-20 16:43:06 +0900

    間隔調整

--- Arduino/PV_reader_9_26/PV_reader_9_26.ino
+++ Arduino/PV_reader_9_26/PV_reader_9_26.ino
@@ -6,6 +6,7 @@ int prev_val = 0;
 int t = 0;
 
 int ascii[8]; //2byte *8 16byte //文字なら010までは固定っぽい //5*8で40 hello用
+int packet[104];
 int pointer = 0; //1khzだと最初読み取れないので1　ふつうは0
 int flag = 0; //最初の確認
 unsigned short crc16 = 0xFFFFU; //
@@ -38,23 +39,33 @@ void loop() { //tの間隔　400 2000 400 2400 400 1600のとき　180-230 180 2
   */
   if (value - prev_val >300) { 
      // これがないと1kHzはむりっぽい
-    Serial.println(t);
+    //Serial.println(t);
     delayMicroseconds(50);
-    if (60<= t && t<70) { //初期化する感じ この値は周期やcbiの設定によっsて変化してしまう
+    if (80<= t && t<110) { //初期化する感じ この値は周期やcbiの設定によっsて変化してしまう
       //pointer = 0; //最後のパルス受信したときに格納先のポインターを初期化しておく
-    }else if(50<= t && t<60 ){ //へんな1を殺す
-       ascii[pointer] = 0; //1or0なら格納
+      Serial.println("初期値");
+    }else if(50<= t && t< 80 ){ //へんな1を殺す
+       //ascii[pointer] = 0; //1or0なら格納
+       packet[pointer] = 0;
        delayMicroseconds(100);
        //Serial.print(ascii[pointer]);
        pointer ++;
-    }else if(t<=120 && t>=95){
-       ascii[pointer] = 1; //1or0なら格納
+       Serial.print("0: ");
+       Serial.println(pointer);
+    }else if(t<=140 && t>=110){
+       //ascii[pointer] = 1; //1or0なら格納
+       packet[pointer] = 1;
        delayMicroseconds(100);
        //Serial.print(ascii[pointer]); //  1/Serial.begin() * (8*文字 + 1 + 1 ) たとえば9600で1文字は1ms程度
        pointer ++;
+       Serial.print("1: ");
+       Serial.println(pointer);
+    }else{
+       Serial.print("その他"); 
     }
-    t =0;
+    t = 0;
   }
+  /*
    if(pointer > 7){
     int len;
     Serial.print("flag:");
@@ -78,6 +89,7 @@ void loop() { //tの間隔　400 2000 400 2400 400 1600のとき　180-230 180 2
        flag = 0; //戻しておく
        }
      }
+    */
   prev_val = value;
 }
 
