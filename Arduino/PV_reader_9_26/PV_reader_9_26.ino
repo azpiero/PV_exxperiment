e8915c5d1c5dfd45166c3dce272ac603ed339be8
azpiero <joek0624@gmail.com>
2016-10-21 14:42:11 +0900

    status

--- Arduino/PV_reader_9_26/PV_reader_9_26.ino
+++ Arduino/PV_reader_9_26/PV_reader_9_26.ino
@@ -10,6 +10,8 @@ int t = 0;
 bool packet[N][8]; //intで保管するとめんどそう
 int p = 0;
 int pointer = 0;
+int status = 0;
+
 
 unsigned short crc16 = 0xFFFFU; //
 
@@ -26,64 +28,67 @@ void setup() {
 }
 
 void loop() { //tの間隔　400 2000 400 2400 400 1600のとき　180-230 180 230
+
   value = analogRead(analogPIN);
   t++;
   /*
   //if(value - prev_val > 300){
-    //Serial.print("t:");
-    //Serial.print(t-1);
-    //Serial.print(" value:");
-    if(value > 500){
-      //Serial.print(t);
-      //Serial.print(",");
-      Serial.println(value);
-      t = 0;
-    }
-  //}
-  */
-  if (value - prev_val >300) {
-     // これがないと1kHzはむりっぽい
-    //Serial.println(t);
+  //Serial.print("t:");
+  //Serial.print(t-1);
+  //Serial.print(" value:");
+  if(value > 500){
+  //Serial.print(t);
+  //Serial.print(",");
+  Serial.println(value);
+  t = 0;
+}
+//}
+*/
+if (value - prev_val >300) {
+  if(status <= 2) {
     delayMicroseconds(50);
     if (80<= t && t<110) { //初期化する感じ この値は周期やcbiの設定によっsて変化してしまう
-      //pointer = 0; //最後のパルス受信したときに格納先のポインターを初期化しておく
       Serial.println("初期値");
-    }else if(50<= t && t< 80 ){ //へんな1を殺す
-       //ascii[pointer] = 0; //1or0なら格納
-       packet[p][pointer] = 0;
-       delayMicroseconds(100);
-       //Serial.print(ascii[pointer]);
-       pointer ++;
-       //Serial.print("0: ");
-       //Serial.println(pointer);
-       p_inc();
-
-    }else if(t<=140 && t>=110){
-       //ascii[pointer] = 1; //1or0なら格納
-       packet[p][pointer] = 1;
-       delayMicroseconds(100);
-       //Serial.print(ascii[pointer]); //  1/Serial.begin() * (8*文字 + 1 + 1 ) たとえば9600で1文字は1ms程度
-       pointer ++;
-       //Serial.print("1: ");
-       //Serial.println(pointer);
-       p_inc();
-
-    }else if(t>200){  //試し
-       show(); //文字見せる リセット
-       for(int i =0;i<N;i++){
-         for(int j =0;j<8;j++){
-         packet[i][j] = 0;
-        }
-       }
-       p = 0;
-       pointer = 0;
+      status ++;
     }
+    }else if(status > 2){
+      if(50<= t && t< 100 ){ //へんな1を殺す
+        //ascii[pointer] = 0; //1or0なら格納
+        packet[p][pointer] = 0;
+        delayMicroseconds(100);
+        //Serial.print(ascii[pointer]);
+        pointer ++;
+        //Serial.print("0: ");
+        //Serial.println(pointer);
+        p_inc();
 
-    t = 0;
+        }else if(t<=140 && t>=100){
+          //ascii[pointer] = 1; //1or0なら格納
+          packet[p][pointer] = 1;
+          delayMicroseconds(100);
+          //Serial.print(ascii[pointer]); //  1/Serial.begin() * (8*文字 + 1 + 1 ) たとえば9600で1文字は1ms程度
+          pointer ++;
+          //Serial.print("1: ");
+          //Serial.println(pointer);
+          p_inc();
+        }
+        else if(t>200){  //試し
+          show(); //文字見せる リセット
+          for(int i =0;i<N;i++){
+            for(int j =0;j<8;j++){
+              packet[i][j] = 0;
+            }
+          }
+          p = 0;
+          pointer = 0;
+          status = 0;
+        }
+        t = 0;
+      }
+
+      prev_val = value;
+    }
   }
-
-  prev_val = value;
-}
 
 void p_inc(){
   if(pointer >7){
